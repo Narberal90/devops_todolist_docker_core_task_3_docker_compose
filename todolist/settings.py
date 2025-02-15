daefba7f3275cded21751dfe07a6f06c0d28a7f4
Narberal90 <mordegear90@gmail.com>
2025-02-15 17:13:39 +0200

    sol2

--- todolist/settings.py
+++ todolist/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("ENGINE"),
+        "ENGINE": "mysql.connector.django",
         "NAME": os.getenv("NAME"),
         "USER": os.getenv("USER"),
         "PASSWORD": os.getenv("PASSWORD"),
